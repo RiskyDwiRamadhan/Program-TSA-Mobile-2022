4e718cf114abff9f555eda31a1895eb50bd280f1
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2022-08-29 21:46:46 +0700

    Cupertino Button dan Loading Bar

--- flutter-fundamental-part1/hello_world/README.md
+++ flutter-fundamental-part1/hello_world/README.md
@@ -8,3 +8,6 @@ A new Flutter project.
 ![Screenshot  Text_Widget](images/02.png)
 ### Image Widget
 ![Screenshot  Image_Widget](images/03.png)
+## Menerapkan Widget Material Design dan iOS Cupertino
+### Cupertino Button dan Loading Bar
+![Screenshot  Cupertino_Button_dan_Loading_Bar](images/04.png)
--- flutter-fundamental-part1/hello_world/lib/main.dart
+++ flutter-fundamental-part1/hello_world/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'basic_widgets/text_widget.dart';
-import 'basic_widgets/image_widget.dart';
+// import 'basic_widgets/image_widget.dart';
+import 'basic_widgets/loading_cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -77,34 +78,35 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const MyImageWidget(),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: MyLoadingCupertinoWidget(),
+      // body: Center(
+      //   // Center is a layout widget. It takes a single child and positions it
+      //   // in the middle of the parent.
+      //   child: Column(
+      //     // Column is also a layout widget. It takes a list of children and
+      //     // arranges them vertically. By default, it sizes itself to fit its
+      //     // children horizontally, and tries to be as tall as its parent.
+      //     //
+      //     // Invoke "debug painting" (press "p" in the console, choose the
+      //     // "Toggle Debug Paint" action from the Flutter Inspector in Android
+      //     // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+      //     // to see the wireframe for each widget.
+      //     //
+      //     // Column has various properties to control how it sizes itself and
+      //     // how it positions its children. Here we use mainAxisAlignment to
+      //     // center the children vertically; the main axis here is the vertical
+      //     // axis because Columns are vertical (the cross axis would be
+      //     // horizontal).
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       MyLoadingCupertinoWidget(),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headline4,
+      //       ),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
