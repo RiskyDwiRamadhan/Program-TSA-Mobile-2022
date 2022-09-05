49357cd1453a9f3c2cbefe92aa57e0f6cf5d11f2
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2022-09-05 22:34:42 +0700

    AutoSizeText

--- flutter_plugin_pubdev/lib/main.dart
+++ flutter_plugin_pubdev/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'red_text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -95,12 +96,19 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              color: Colors.yellowAccent,
+              width: 50,
+              child: const RedTextWidget(
+                text: 'You have pushed the button this many times:',
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              color: Colors.greenAccent,
+              width: 100,
+              child: const Text(
+                'You have pushed the button this many times:',
+              ),
             ),
           ],
         ),
